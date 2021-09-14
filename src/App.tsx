95b6794dc1b2bbf8f3efb3bc429663b8e8759123
Piotr Marian Jan Gondek <piotrgondek+github@protonmail.com>
2021-09-14 09:39:07 +0200

    feat(13): introduce Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, CssBaseline } from '@material-ui/core';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img
-        src={logo}
-        className="App-logo"
-        alt="logo"
-      />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <>
+    <CssBaseline />
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </>
 );
 
 export default App;
